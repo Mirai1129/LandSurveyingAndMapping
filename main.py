6009bd2edaf158ab774c16d30bc6ec6da9b5ceec
Mirai1129 <minecraftmiku831@gmail.com>
2024-07-12 15:08:47 +0800

    :rocket: add usage

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-# main.py
-from modules.region_processor import process_region_code
 from scripts import CSVProcessor, CadasMapPosition, EasymapCrawler
 
 
 def main():
-    csv_processor = CSVProcessor()
-    cadas_processor = CadasMapPosition()
-    easymap_crawler = EasymapCrawler()
-    matching_region_codes = csv_processor.list_matching_files()
-    print(f"Found matching region codes: {matching_region_codes}")
+    processor = CSVProcessor()
+    crawler = EasymapCrawler()
+    cadas_position = CadasMapPosition()
 
-    for region_code in matching_region_codes:
-        process_region_code(region_code, csv_processor)
+    for region_code in processor.list_matching_files():
+        df = processor.read_csv_files(region_code)
+
+        if df is not None:
+            for index, row in df.iterrows():
+                city_name = processor.get_county_name(region_code)
+                township_name = row['region_name']
+                section_number = row['section']
+                land_number = row['land_serial_number']
+
+                if section_number and land_number:
+                    town_code = processor.find_towncode(city_name, township_name, section_number, row['sub_section'])
+                    if town_code:
+                        land_number_found = crawler.run_process_flow_to_get_land_number(
+                            city_name,
+                            township_name,
+                            section_number,
+                            land_number
+                        )
+                        if land_number_found:
+                            cadas_position.set_parameters(town_code, section_number, land_number_found)
+                            position_data = cadas_position.fetch_position()
+                            if position_data:
+                                df.at[index, 'repX'] = position_data['repX']
+                                df.at[index, 'repY'] = position_data['repY']
+                                df.at[index, 'ldX'] = position_data['ldX']
+                                df.at[index, 'ldY'] = position_data['ldY']
+                                df.at[index, 'rtX'] = position_data['rtX']
+                                df.at[index, 'rtY'] = position_data['rtY']
+
+            processor.save_results_to_csv(df, f"{region_code}_lvr_land_a.csv")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- scripts/CadasMapPosition.py
+++ scripts/CadasMapPosition.py
@@ -13,8 +13,10 @@ class CadasMapPosition:
         self.land_number = None  # 地號
         self.url = None  # API請求的URL
         self.header = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/100.0.4896.75 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,'
+                      '*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6',
             'Accept-Encoding': 'gzip, deflate, br',
             'DNT': '1',
